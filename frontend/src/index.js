8315da65efc1b8c85a19e8494dfce6a294fc8edc
Madhav Patel <madhavpatel249@gmail.com>
2025-06-21 13:50:27 -0400

    fix: rendering issues

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,11 +7,9 @@ import { CartProvider } from './components/CartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <AuthProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </AuthProvider>
-  </React.StrictMode>
+  <AuthProvider>
+    <CartProvider>
+      <App />
+    </CartProvider>
+  </AuthProvider>
 );
